3a65ed17d2f20f826b8eefa0a2c976e64551e246
Michael Sumner <mdsumner@gmail.com>
2017-07-01 09:42:50 +1000

    date

--- tests/testthat/test-ice-files.R
+++ tests/testthat/test-ice-files.R
@@ -4,8 +4,8 @@ s <- nsidc_south_monthly_files()
 n <- nsidc_north_monthly_files()
 a <- nsidc_monthly_files()
 test_that("file stamps are sensible", {
-  dts <- range(diff(s$date))
-  dtn <- range(diff(n$date))
+  dts <- unclass(range(diff(as.Date(s$date))))
+  dtn <- unclass(range(diff(as.Date(n$date))))
   if (nrow(s) > 1) expect_gt(min(dts),  27)
   if (nrow(n) > 1) expect_gt(min(dtn),  27)
 
