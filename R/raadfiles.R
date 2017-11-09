0eb1bda9ef97f97debd9161a586a4e6296c7913c
Michael Sumner <mdsumner@gmail.com>
2017-11-09 15:10:55 +1100

    apply local dir to all files

--- R/raadfiles.R
+++ R/raadfiles.R
@@ -1,5 +1,8 @@
 get_raw_raad_filenames <- function() {
-  getOption("raadfiles.filename.database" )
+  out <- getOption("raadfiles.filename.database" )
+  base <- basename(out$root)
+  out$root <- file.path(get_raad_datadir(), base)
+  out
 }
 get_raad_datadir <- function() {
   getOption("raadfiles.default.data.directory")
