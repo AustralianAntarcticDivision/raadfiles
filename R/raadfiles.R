8d0bb608750aa1713f00907c3bdfc81c0ecdceed
Michael Sumner <mdsumner@gmail.com>
2017-11-10 11:04:15 +1100

    roll back path hack

--- R/raadfiles.R
+++ R/raadfiles.R
@@ -1,7 +1,7 @@
 get_raw_raad_filenames <- function() {
   out <- getOption("raadfiles.filename.database" )
-  base <- basename(out$root)
-  out$root <- file.path(get_raad_datadir(), base)
+ # base <- basename(out$root)
+#  out$root <- file.path(get_raad_datadir(), base)
   out
 }
 get_raad_datadir <- function() {
