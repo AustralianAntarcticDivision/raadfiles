18f504b5ce64b0d54c8246b05cd6c01cd56cd416
Michael Sumner <mdsumner@gmail.com>
2017-12-07 11:43:03 +1100

    Update raadfiles.R

--- R/raadfiles.R
+++ R/raadfiles.R
@@ -1,7 +1,9 @@
 get_raw_raad_filenames <- function() {
   out <- getOption("raadfiles.filename.database" )
- # base <- basename(out$root)
-#  out$root <- file.path(get_raad_datadir(), base)
+  if (.Platform$OS.type == "windows") {
+  base <- basename(out$root)
+  out$root <- file.path(get_raad_datadir(), base)
+    }
   out
 }
 get_raad_datadir <- function() {
