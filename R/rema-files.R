ce183c7d38ad40158072ea124384632e0cc5fd3d
Michael Sumner <mdsumner@gmail.com>
2018-09-13 16:56:31 +1000

    fix rema, add index

--- R/rema-files.R
+++ R/rema-files.R
@@ -10,6 +10,12 @@
 }
 #' @name rema_8m_files
 #' @export
+rema_tile_files <- function(all = FALSE, ...) {
+  pat <- if (all) "Tile_index_" else "Tile_Index_.*shp$"
+  .rema_all_files(all = TRUE) %>% dplyr::filter(stringr::str_detect(.data$file, pat))
+}
+#' @name rema_8m_files
+#' @export
 rema_100m_files <- function(filled = TRUE, ...) {
   pat <- if (filled) "100m_.*filled" else "100m_"
   files <- .rema_all_files() %>% dplyr::filter(stringr::str_detect(.data$file, pat))
@@ -20,7 +26,7 @@ rema_100m_files <- function(filled = TRUE, ...) {
 #' @name rema_8m_files
 #' @export
 rema_200m_files <- function() {
-  pat <- if (filled) "200m_.*filled" else "200m_"
+  pat <- "200m_"
   files <- .rema_all_files() %>% dplyr::filter(stringr::str_detect(.data$file, "200m"))
   if (nrow(files) < 1)
     stop("no files found")
@@ -29,7 +35,7 @@ rema_200m_files <- function() {
 #' @name rema_8m_files
 #' @export
 rema_1km_files <- function() {
-  pat <- if (filled) "1km_.*filled" else "1km_"
+  pat <- "1km_"
   files <- .rema_all_files() %>% dplyr::filter(stringr::str_detect(.data$file, "1km"))
   if (nrow(files) < 1)
     stop("no files found")
@@ -40,14 +46,14 @@ rema_1km_files <- function() {
 #'
 #' Return files for various products from REMA Release 1
 #'
-#' 'rema_8m_files' returns the base level 8 GeoTIFF files, there are 1254 files at 8m resolution.
+#' 'rema_8m_files' returns the base level 8 GeoTIFF files, there are 1516 files at 8m resolution.
 #' @references \url{https://www.pgc.umn.edu/tag/rema/}
 #' @param filled return 'filled' variant if available
 #' @param ...
 #'
 #' @return data frame of file names
 #' @export
-#' @aliases rema_100m_files rema_200m_files rema_1km_files
+#' @aliases rema_100m_files rema_200m_files rema_1km_files rema_tile_files
 #' @examples
 #' rema_8m_files()
 #' rema_100m_files(filled = TRUE)
