2e807e3a3adee7769fd0b6496c796c57b818b043
Michael Sumner <mdsumner@gmail.com>
2019-02-13 04:45:59 +0000

    argo files

---
+++ R/raad-argo-files.R
@@ -0,0 +1,30 @@
+#' Argo files.
+#'
+#' ARGO
+#' @param type file type, i.e. prof, meta, traj, tech
+#' @param dac data acqusition centre e.g.* "aoml", "bodc", "coriolis", "csio", "csiro", "incois", all returned if not specified
+#' @export
+#' @references [https://www.aviso.altimetry.fr/en/data/products/value-added-products/fsle-finite-size-lyapunov-exponents.html]( Finite-Size Lyapunov Exponents)
+argo_files <- function(type = c("prof", "meta", "traj", "tech", "Mprof"), dac = NULL) {
+  type <- match.arg(type)
+  files <- dplyr::filter(get_raad_filenames(), stringr::str_detect(.data$file, "usgodae"))
+  files <- dplyr::filter(get_raad_filenames(), stringr::str_detect(.data$file, "nc$"))
+  files <- dplyr::filter(files, stringr::str_detect(.data$file, "/argo/"))
+
+
+  if (!is.null(dac)) {
+    files <- dplyr::filter(get_raad_filenames(), stringr::str_detect(.data$file, sprintf("/%s/", dac)))
+  }
+  typefilter <- switch(type,
+                       prof = "profiles")
+
+  files <- dplyr::filter(files, stringr::str_detect(.data$file, typefilter))
+
+
+  if (nrow(files) < 1)
+    stop("no files found")
+  files <-   dplyr::transmute(files, fullname = file.path(.data$root, .data$file), .data$root)
+
+  files
+}
+
