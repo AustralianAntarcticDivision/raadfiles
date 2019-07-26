29c6d4848992907af58b428c9bad086c16ac86f4
Michael Sumner <mdsumner@gmail.com>
2019-07-26 12:09:17 +1000

    update tests

--- tests/testthat/test-list-data.R
+++ tests/testthat/test-list-data.R
@@ -2,12 +2,12 @@ context("list-data")
 
 test_that("list files found", {
   skip_on_travis()  ## we need bowerbird
-  thelist_files(format = "")  %>%   expect_named(c("file", "fullname")) ## get all files
+  thelist_files(format = "")  %>%   expect_named(c("fullname", "root")) ## get all files
   thelist_files(format = "tab", pattern = "tab$")  %>% expect_s3_class("tbl_df") ## those kind
   parcels <- thelist_files(pattern = "parcel")
   cparcels <- thelist_files(pattern = "parcels_c")
   expect_true(nrow(parcels) > nrow(cparcels))
-  thelist_files(format = "tab") %>% expect_named(c("file", "fullname"))
+  thelist_files(format = "tab") %>% expect_named(c("fullname", "root"))
 })
 
 test_that("read em all", {
--- tests/testthat/test-ncep2.R
+++ tests/testthat/test-ncep2.R
@@ -2,10 +2,10 @@ context("ncep2")
 
 test_that("NCEP2 files good", {
  uwnd <- ncep2_uwnd_6hr_files()
- uwnd %>% expect_s3_class("tbl_df") %>% expect_named(c("file", "fullname", "date"))
+ uwnd %>% expect_s3_class("tbl_df") %>% expect_named(c("date", "fullname", "root"))
  expect_true(inherits(uwnd$date, "POSIXct"))
- expect_true(all(grepl("uwnd", basename(uwnd$file))))
+ expect_true(all(grepl("uwnd", basename(uwnd$fullname))))
  vwnd <- ncep2_vwnd_6hr_files()
- expect_true(all(grepl("vwnd", basename(vwnd$file))))
+ expect_true(all(grepl("vwnd", basename(vwnd$fullname))))
 
 })
