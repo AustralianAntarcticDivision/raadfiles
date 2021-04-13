092aca6f356477ee2e0862f6546b1a7fd9557ce1
Michael Sumner <mdsumner@gmail.com>
2021-04-13 04:31:09 +0000

    add all parm

--- R/raad-altimetry-files.R
+++ R/raad-altimetry-files.R
@@ -40,9 +40,16 @@ altimetry_currents_polar_files <- function(hemisphere = "south") {
 #' sea surface height and derived variables measured by multi-satellite altimetry observations over
 #' Global Ocean.
 #'
-#' In 2018/2019 the file servers migrated to 'my.cmems-du.au' and 'nrt.cmems-du.eu' from 'ftp.sltac.cls.fr', but
+#' In 2018/2019 the file servers migrated to 'my.cmems-du.au' and 'nrt.cmems-du.eu' (NRT) from 'ftp.sltac.cls.fr', but
 #' the files and file name scheme remained unchanged so no net effect (so far that we are aware of).
-#' @return tibble data frame of file names
+#'
+#' There are NRT (near-real-time) and final processed files, identifiable from the root domain and in the filename '^nrt_'. Both
+#' are returned by this function. The `all` argument can be set to `TRUE` to include all NRT files, which has overlapping processing dates
+#' that are ultimately consolidated into the daily sequence.
+#'
+#' @param all return all files or only the final processing (NRT is included either way)
+#'
+#' @return tibble data frame of file names, data 'date', and 'processing_date'
 #' @importFrom dplyr arrange distinct filter mutate
 #' @importFrom rlang .data
 #' @name altimetry
@@ -50,7 +57,7 @@ altimetry_currents_polar_files <- function(hemisphere = "south") {
 #' @importFrom stringr str_detect str_extract str_replace
 #' @examples
 #' altimetry_daily_files()
-altimetry_daily_files <- function() {
+altimetry_daily_files <- function(all = FALSE) {
 #  SSH etc now come from Copernicus, in a different format. Files
 #  reside under
 #  ftp.sltac.cls.fr/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046
@@ -63,20 +70,30 @@ altimetry_daily_files <- function() {
   files <- dplyr::filter(get_raad_filenames(), stringr::str_detect(.data$file, "SEALEVEL_GLO_PHY_L4"))
   files <- dplyr::filter(files, stringr::str_detect(.data$file, "nc$"))  ## faster without the .
 
-  files <-   dplyr::transmute(files, fullname = file.path(.data$root, .data$file), root = .data$root,
-                              date = as.POSIXct(as.Date(stringr::str_extract(basename(.data$fullname), "[0-9]{8}"),
-                                                        "%Y%m%d"),tz = "GMT"))
+  files <-   dplyr::transmute(files, fullname = file.path(.data$root, .data$file), root = .data$root)
+  dates <- stringr::str_extract(basename(files$fullname), "[0-9]{8}_[0-9]{8}")
+
+  files$date <- as.POSIXct(as.Date(stringr::str_extract(dates, "[0-9]{8}"),"%Y%m%d"),tz = "UTC")
+  files$processing_date <- as.POSIXct(as.Date(stringr::str_extract(unlist(lapply(strsplit(dates, "_"), "[", 2L)),
+                                                                   "[0-9]{8}"),"%Y%m%d"),tz = "UTC")
+
+
 
   if (nrow(files) < 1)
     stop("no files found")
 
-  files <- dplyr::transmute(files, date = .data$date, fullname= .data$fullname, root = .data$root)
+  files <- dplyr::transmute(files, date = .data$date, fullname= .data$fullname, root = .data$root, .data$processing_date)
   bad <- is.na(files$date)
   if (sum(bad) == 1 && which(bad) == nrow(files)) {
     files$date[bad] <- max(files$date, na.rm = TRUE) + 24 * 3600
   } else {
     files <- files[!is.na(files$date), ]
   }
-  dplyr::arrange(dplyr::distinct(files, .data$date, .keep_all = TRUE), .data$date)   %>%
-    set_dt_utc()
+  if (!all) {
+    files <- dplyr::arrange(dplyr::distinct(files, .data$date, .keep_all = TRUE), .data$date)
+  } else {
+    files <- dplyr::arrange(files, .data$date, .data$processing_date)
+  }
+
+ files
 }
